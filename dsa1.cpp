487180fe5c619a48d2967f8511397d6054a62701
DikshaNautiyal06 <nautiyaldiksha42@gmail.com>
2024-08-28 16:12:45 +0530

    ii

--- dsa1.cpp
+++ dsa1.cpp
@@ -1,8 +1,13 @@
+
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
-int main(){
-    cout<<"Hello";
-    return 0;
+int main() {
+   for(int i=1;i<=5;i++){
+    for(int j=1;j<=i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+
+   }
 }
